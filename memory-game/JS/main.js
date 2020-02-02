bd6f237dcb144162061066e98a7d39caf526f4c9
Sean O'brien <seanobrien@Rover-SeanObrien.local>
2020-02-01 20:05:14 -0800

    created game logic

--- memory-game/JS/main.js
+++ memory-game/JS/main.js
@@ -1,16 +1,22 @@
 const cards = ["queen", "queen", "king", "king"];
 cardsInPlay = [];
 
-let cardOne = cards[0]; 
-cardsInPlay.push(cardOne);
-let cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You have a match!");
-	}
-	else {
-		alert("Sorry, try again.");
+function checkForMatch() {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			console.log("You found a match!");
+		}
+		else {
+			console.log("Sorry, try again.");
+		}
 	}
 }
+
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+
+flipCard(0)
+flipCard(2)
